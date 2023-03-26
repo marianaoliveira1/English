2a79a154d52ce4ef51a367638df95087f6eb5d9f
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2023-03-26 09:25:00 -0300

    screen de bichos

--- lib/screens/bichos_screen.dart
+++ lib/screens/bichos_screen.dart
@@ -15,7 +15,28 @@ class _BichosScreenState extends State<BichosScreen> {
       children: [
         GestureDetector(
           onTap: () {},
-        )
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/vaca.png"),
+        ),
       ],
     );
   }
